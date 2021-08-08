5fec403b30ed4c583e20140600ba08878b7babd8
LeonarDev <leonardo.majevski@gmail.com>
2021-08-08 20:09:15 -0300

    Inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inheritance.Entities;
+using System;
 
 namespace Inheritance
 {
@@ -6,7 +7,11 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.0; // error
         }
     }
 }
